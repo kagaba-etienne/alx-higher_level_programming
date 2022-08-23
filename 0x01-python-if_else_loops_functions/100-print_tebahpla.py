644af48e3c255ddd09359239e55d59ca4e5b965b
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-24 00:04:30 +0200

    remove at position

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(122,96,-2):
+    print("{0:c}{1:c}".format(i, i - 33), end='')
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    num = 0
+    copy_str = ""
+    for i in str:
+        j++
+    for index in range(j):
+        if index == n:
+            continue
+        copy_str += str[index]
+    return copy_str
