70e17242bb26decc4071f8a82665d32483c4c94e
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-23 20:49:12 +0200

    debugged

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
     print(f"{i:02d}", end=', ')
-print(f"99")
+print(f"{99:d}")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(8):
     for j in range(i+1, 10):
-        print(f"{i}{j}", end=', ')
-print(f"89")
+        print(f"{i:d}{j:d}", end=', ')
+print(f"{89:d}")
