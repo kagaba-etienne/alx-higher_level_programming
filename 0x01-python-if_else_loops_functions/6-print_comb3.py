aff64f3ffb8a45c2598d759f766cd91deb9cf9d9
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-23 21:05:47 +0200

    debugged

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(8):
+for i in range(9):
     for j in range(i+1, 10):
-        print(f"{0:d}{1:d}".format(i, j), end=', ')
-print("{:d}".format(89))
+        if i == 8:
+            print("{0:d}{1:d}".format(i, j))
+            break
+        print("{0:d}{1:d}".format(i, j), end=', ')
