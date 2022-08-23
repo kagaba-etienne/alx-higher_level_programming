9fb612106128384d919cb294a02cee51ac394e16
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-23 21:01:49 +0200

    debugged

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
+for i in range(100):
+    if i == 99:
+        print ("{:d}".format(i))
+        break
     print("{:02d}".format(i), end=', ')
-print(":d}".format(99))
