e87d6d999f71d700b84b66069d6f79b9e4b8ed9e
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-23 21:32:55 +0200

    prints the last digits

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         last_digit = (number % 10)
     else:
         last_digit = 10 - (number % 10)
-    print("{0:d}".format(last_digit))
+    print("{0:d}".format(last_digit), end='')
     return last_digit
