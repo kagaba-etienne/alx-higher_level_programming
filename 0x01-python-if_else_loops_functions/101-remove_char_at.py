f6d14a5ae9edecaf15a8c2e3522f569f2c697927
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-24 00:07:46 +0200

    remove at position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,8 +3,8 @@ def remove_char_at(str, n):
     num = 0
     copy_str = ""
     for i in str:
-        j += 1
-    for index in range(j):
+        num += 1
+    for index in range(num):
         if index == n:
             continue
         copy_str += str[index]
