0412ff71ed203920db5c397375fb8f527f2b685e
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-25 18:45:29 +0200

    Build my own calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    import calculator_1 as cal
     length = len(sys.argv)
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
@@ -13,12 +14,12 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
         match sys.argv[2]:
             case '+':
-                print("{:d} + {:d} = {:d}".format(a, b, a + b))
+                print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
             case '/':
-                print("{:d} / {:d} = {}".format(a, b, a / b))
+                print("{:d} / {:d} = {}".format(a, b, cal.div(a, b)))
             case '-':
-                print("{:d} - {:d} = {}".format(a, b, a - b))
+                print("{:d} - {:d} = {}".format(a, b, cal.sub(a, b)))
             case '*':
-                print("{:d} * {:d} = {}".format(a, b, a * b))
+                print("{:d} * {:d} = {}".format(a, b, cal.mul(a, b)))
             case _:
                 pass
