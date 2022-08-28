5c3d448d4fdd930cd50c380ed34a1ccb8f0010df
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-28 20:02:15 +0200

    More returns!

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -6,5 +6,5 @@ tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+print(add_tuple(tuple_a, (1,)))
+print(add_tuple(tuple_a, (2,)))
