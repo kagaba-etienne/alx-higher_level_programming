a97b8d3307ee013b70249b0eaa0f07436d68c1bb
kagaba-etienne <kagabaetienne04@gmail.com>
2022-08-28 19:53:03 +0200

    tupples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -11,7 +11,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         newt = (tuple_a[0]), (tuple_a[1]),
     elif len1 == 1 and len2 > 1:
         newt = (tuple_a[0] + tuple_b[0]), (tuple_b[1]),
-    elif len1 == 0 and len2 >1:
+    elif len1 == 0 and len2 > 1:
         newt = (tuple_b[0]), (tuple_b[1]),
     else:
         pass
